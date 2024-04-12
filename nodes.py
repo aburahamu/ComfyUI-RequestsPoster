d01138d2024d573e805b06fa63be11f7cddd6ba1
aburahamu <atsushi.aburahamu@gmail.com>
2024-04-13 00:53:11 +0900

    ver.0.15

--- nodes.py
+++ nodes.py
@@ -1,39 +1,22 @@
 import requests
 
-class RequestSettings:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "text_url": ("STRING", {"multiline": False}),
-                "text_key": ("STRING", {"multiline": False}),
-                "text_value": ("STRING", {"multiline": False})
-            }
-        }
-    RETURN_TYPES = ("text_url", "text_key", "text_value")
-    FUNCTION = "run"
-    CATEGORY = "RequestSettings"
-
-    def run(self, text_url, text_key, text_value):
-        return (text_url, text_key, text_value)
-
 class PostRequests:
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "text_url": ("STRING", {"forceInput": True, "default": "https://~"}),
-                "text_key": ("STRING", {"forceInput": True, "default": "content"}),
-                "text_value": ("STRING", {"forceInput": True, "default": "Hello, World!!"}),
+                "text_url": ("STRING", {"multiline": False, "default": "https://~"}),
+                "text_key": ("STRING", {"multiline": False, "default": "content"}),
+                "text_value": ("STRING", {"multiline": False, "default": "Hello, World!!"}),
                 "image": ("IMAGE",),
             }
         }
     
     RETURN_TYPES = ()
     FUNCTION = "run"
-    CATEGORY = "RequestsPoster"
+    CATEGORY = "RequestSettings"
 
-    def run(self, text_url, text_key, text_value):
+    def run(self, text_url, text_key, text_value, image):
         url = text_url
         data = {text_key: text_value}
         response = requests.post(url, data=data)
@@ -43,11 +26,9 @@ class PostRequests:
         return (response.text)
 
 NODE_CLASS_MAPPINGS = {
-    "RequestSettings": RequestSettings,
     "PostRequests": PostRequests,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "RequestsPoster": "RequestSettings",
     "RequestsPoster": "PostRequests",
 }
