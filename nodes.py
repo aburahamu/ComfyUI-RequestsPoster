95438dcacc4c1f309270ff74cf18efc90ed8a0a6
aburahamu <atsushi.aburahamu@gmail.com>
2024-04-14 19:19:48 +0900

    Add PostImage2X

--- nodes.py
+++ nodes.py
@@ -1,4 +1,11 @@
 import requests
+import tweepy
+import json
+import folder_paths
+import numpy as np
+import os
+from PIL import Image
+from datetime import datetime
 
 class AnyType(str):
     def __ne__(self, __value: object) -> bool:
@@ -6,6 +13,7 @@ class AnyType(str):
 
 any = AnyType("*")
 
+# PostText ***********************************************************************************
 class PostText:
     def __init__(self):
         pass
@@ -34,10 +42,86 @@ class PostText:
         print(response)
         return ()
 
+# PostImage2X ***********************************************************************************
+class PostImage2X:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+        self.compress_level = 4
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "images": ("IMAGE", ),
+                "filename_prefix": ("STRING", {"default": "ComfyUI"}),
+                "text_message": ("STRING", {"multiline": True, "default": "Image generation is complete."}),
+                "text_consumer_key": ("STRING", {"multiline": False}),
+                "text_consumer_secret": ("STRING", {"multiline": False}),
+                "text_access_token": ("STRING", {"multiline": False}),
+                "text_access_token_secret": ("STRING", {"multiline": False}),
+            },
+        }
+
+    FUNCTION = "run"
+    OUTPUT_NODE = True
+    RETURN_TYPES = ()
+
+    CATEGORY = "RequestsPoster"
+
+    # 画像を保存する関数(SaveImageをコピペした)
+    def GetSaveImagePath(self, images, filename_prefix):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(
+            filename_prefix, 
+            self.output_dir, 
+            images[0].shape[1], 
+            images[0].shape[0]
+        )
+
+        image = images[0]
+        i = 255. * image.cpu().numpy()
+        img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+        metadata = None
+
+        filename = datetime.now().strftime("%Y%m%d-%H%M%S") + ".png"
+        fullpath = os.path.join(full_output_folder, filename)
+        img.save(fullpath, pnginfo=metadata, compress_level=self.compress_level)
+
+        return fullpath
+
+    # Xに投稿する関数
+    def run(self, images, text_message, text_consumer_key, text_consumer_secret, text_access_token, text_access_token_secret, filename_prefix="comfyUI"):
+        # TweepyのAPIオブジェクトを作成
+        auth = tweepy.OAuthHandler(text_consumer_key, text_consumer_secret)
+        auth.set_access_token(text_access_token, text_access_token_secret)
+
+        # TweepyのAPIとClientオブジェクトを作成
+        api = tweepy.API(auth)
+        client = tweepy.Client(
+            consumer_key = text_consumer_key, 
+            consumer_secret = text_consumer_secret, 
+            access_token= text_access_token, 
+            access_token_secret= text_access_token_secret
+        )
+
+        # 画像を投稿
+        image_path = self.GetSaveImagePath(images=images, filename_prefix=filename_prefix)
+        message = text_message
+        media = api.media_upload(filename=image_path)
+        result = client.create_tweet(text=message, media_ids=[media.media_id])
+
+        print(result)
+        return ()
+    
+# Mappings ***********************************************************************************
 NODE_CLASS_MAPPINGS = {
-    "PostText": PostText
+    "PostText": PostText,
+    "PostImage2X": PostImage2X
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PostText": "PostText"
+    "PostText": "PostText",
+    "PostImage2X": "PostImage2X"
 }
